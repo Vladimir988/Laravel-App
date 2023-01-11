48889fdcaf562c8bc3cb238fccf1f9098181a58a
Vladimir988 <optimist0706@gmail.com>
2023-01-11 21:20:14 +0200

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::group(['namespace' => 'Post'], function () {
 Route::get('/index', 'IndexController@index')->name('main.index');
 Route::get('/about', 'AboutController@index')->name('about.index');
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'admin'], function () {
     Route::group(['namespace' => 'Post'], function () {
         Route::get('/post', 'IndexController')->name('admin.post.index');
     });
