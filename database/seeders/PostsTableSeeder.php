287973f800c1cdb06a69a6c488d8877e344142cd
Vladimir988 <optimist0706@gmail.com>
2023-02-14 13:28:23 +0200

    app apdated to laravel 8.75

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php namespace Database\Seeders;
+
+use App\Post;
+use App\Tag;
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Category::class, 20)->create();
+        $tags  = factory(Tag::class, 30)->create();
+        $posts = factory(Post::class, 200)->create();
+
+        foreach ($posts as $post) {
+            $tagsIds = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagsIds);
+        }
+    }
+}
