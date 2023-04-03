e5c15302fdf010fb0ac42afbdb0d64af63ac9f2f
Vladimir988 <optimist0706@gmail.com>
2023-04-03 21:56:45 +0300

    main blog page assets + router

--- routes/web.php
+++ routes/web.php
@@ -11,35 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::group(['namespace' => 'Post'], function () {
-    Route::get('/posts', 'IndexController')->name('post.index');
-    Route::get('/posts/create', 'CreateController')->name('post.create');
-    Route::post('/posts', 'StoreController')->name('post.store');
-    Route::get('/posts/{post}', 'ShowController')->name('post.show');
-    Route::get('/posts/{post}/edit', 'EditController')->name('post.edit');
-    Route::patch('/posts/{post}', 'UpdateController')->name('post.update');
-    Route::delete('/posts/{post}', 'DestroyController')->name('post.delete');
-});
-
-Route::get('/home', 'HomeController@index')->name('home.index');
-Route::get('/about', 'AboutController@index')->name('about.index');
-
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'admin'], function () {
-    Route::group(['namespace' => 'Post'], function () {
-        Route::get('/post', 'IndexController')->name('admin.post.index');
-    });
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', 'IndexController');
 });
 
 Auth::routes();
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
