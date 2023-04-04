4b1edeee05ce3fa16cdf6a9ab458a73465d53cf7
Vladimir988 <optimist0706@gmail.com>
2023-04-04 16:19:38 +0300

    AdminLTE v3.2.0

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController');
 });
 
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+});
+
 Auth::routes();
