90503963e43e4f40b9856ac8a4eeabc543da1fd2
Vladimir988 <optimist0706@gmail.com>
2023-04-05 12:02:28 +0300

    store controller, view, rout

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -10,11 +10,15 @@
                             <div class="card-header">
                                 <h3 class="card-title">Add category</h3>
                             </div>
-                            <form action="#">
+                            <form action="{{ route('admin.category.store') }}" method="POST">
+                                @csrf
                                 <div class="card-body">
                                     <div class="form-group">
                                         <label for="category-name">Category name</label>
-                                        <input type="text" class="form-control" id="category-name" placeholder="Category name">
+                                        <input type="text" class="form-control" id="category-name" name="title" placeholder="Category name">
+                                        @error('title')
+                                            <div class="text-danger">Category name is required!</div>
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="card-footer">
