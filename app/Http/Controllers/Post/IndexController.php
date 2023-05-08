f750b624c87559af1355ab2bcef0ee6d0bbb666f
Vladimir988 <optimist0706@gmail.com>
2023-05-08 18:13:27 +0300

    Laravel telescope

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -9,6 +9,7 @@ class IndexController extends Controller
 {
         public function __invoke()
     {
+//      $posts       = Post::with('category')->paginate(3);
         $posts       = Post::paginate(3);
         $randomPosts = Post::inRandomOrder()->limit(10)->get();
         return view('post.index', compact('posts', 'randomPosts'));
